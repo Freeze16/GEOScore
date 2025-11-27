707f13d3a8b389d6795d7107846b3521bf4145e7
Der_computer <tafince16.05@gmail.com>
2025-11-27 15:37:41 +0300

    Removing unecesrarry dependenices

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
 from fastapi import FastAPI, Request, Form
-from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-
 import time
 
-app = FastAPI(title="GEOScore", version="0.0.1")
+app = FastAPI(title="GEOScore", version="0.1.0")
 
 # Монтируем статические файлы и шаблоны
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
-
-@app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 
-@app.post("/analyze", response_class=HTMLResponse)
+@app.post("/analyze")
 async def analyze_url(request: Request, url: str = Form(...)):
     # Имитируем обработку
     time.sleep(1)
 
-    return templates.TemplateResponse("index.html", {
-        "request": request,
-        "url": url,
-    })
+    return { "id": 1 }
 
 if __name__ == "__main__":
     import uvicorn
