ace79871466b79dc494a89c0ea87ac6b0c7889d5
Freeze16 <gnetov.i@mail.ru>
2025-11-27 15:07:11 +0300

    Basic web interface

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("hello world")
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+import time
+
+app = FastAPI(title="GEOScore", version="0.0.1")
+
+# Монтируем статические файлы и шаблоны
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/analyze", response_class=HTMLResponse)
+async def analyze_url(request: Request, url: str = Form(...)):
+    # Имитируем обработку
+    time.sleep(1)
+
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "url": url,
+    })
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
